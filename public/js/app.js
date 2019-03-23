3dec96696f57874c215bb71113a1ef752356cf6b
Eileen Kuo <j405307@gmail.com>
2019-03-23 23:08:07 +0800

    fix 2

--- public/js/app.js
+++ public/js/app.js
@@ -26,7 +26,7 @@ wearther_form.addEventListener('submit', (e) => {
     message_one.textContent = 'Loading...'
     message_two.textContent = ''
 
-    fetch('/weather?address=' + encodeURIComponent(location)).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 message_one.textContent = data.error
